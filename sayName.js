eb2c305024c25a5e7d594384a19311a7c8294638
Andrew Jenkins <stumptownandy@gmail.com>
2015-10-01 18:21:02 -0600

    cool

--- sayName.js
+++ sayName.js
@@ -1,7 +1,12 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
   //code here
-
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+var myFather = new Person("Aaron",72);
+console.log (myFather);
 
 //Now create three instances of Person with data you make up
 
